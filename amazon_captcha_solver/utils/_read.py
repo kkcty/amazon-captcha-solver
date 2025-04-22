6183b323e4d0a0766bf3f48c79d6064486f70878
kkcty <jdycty2002@163.com>
2025-04-22 17:21:40 +0800

    remove read_image_async

--- amazon_captcha_solver/utils/_read.py
+++ amazon_captcha_solver/utils/_read.py
@@ -4,16 +4,10 @@ from io import BytesIO, RawIOBase, BufferedIOBase
 from pathlib import Path
 from typing import TYPE_CHECKING, overload, cast
 
-from aiofiles import open as async_open
 from PIL import Image as ImageModule
 
 if TYPE_CHECKING:
-    from typing import Awaitable, Literal
-
     from ..type_hints import BinaryIO, Image, ImageSource, StrPath
-
-
-sync_open = open
 
 
 def check_file_before_read(f: StrPath, /) -> Path:
@@ -30,55 +24,34 @@ def check_file_before_read(f: StrPath, /) -> Path:
 
 
 @overload
-async def read_image(
-    d: StrPath, async_mode: Literal[True], /, *, formats: list[str] | None = None
-) -> Image: ...
+def read_image(s: StrPath, /, *, formats: list[str] | None = None) -> Image: ...
 @overload
-def read_image(d: StrPath, async_mode: Literal[False], /, *, formats: list[str] | None = None) -> Image: ...
+def read_image(s: BinaryIO, /, *, formats: list[str] | None = None) -> Image: ...
 @overload
-def read_image(d: BinaryIO, async_mode: None = None, /, *, formats: list[str] | None = None) -> Image: ...
-@overload
-def read_image(d: bytes, async_mode: None = None, /, *, formats: list[str] | None = None) -> Image: ...
-def read_image(
-    d: ImageSource, async_mode: bool | None = None, /, *, formats: list[str] | None = None
-) -> Image | Awaitable[Image]:
+def read_image(s: bytes, /, *, formats: list[str] | None = None) -> Image: ...
+def read_image(s: ImageSource, /, *, formats: list[str] | None = None) -> Image:
     """
     read image from file, bytes or binary-io
 
-    :param d: file-path, bytes or binary-io
-    :type d: Path | str | bytes | RawIOBase | BufferedIOBase
-    :param async_mode: enable async (file-path only)
-    :type async_mode: bool | None
+    :param s: file-path, bytes or binary-io
+    :type s: Path | str | bytes | RawIOBase | BufferedIOBase
     :param formats: possible image formats
     :type formats: list[str] | None
 
     :return: Pillow Image
-    :rtype: Image | Awaitable[Image]
+    :rtype: Image
     """
-    # TODO: support image-base64 in future
+    # TODO: support image-base64 in future.
 
     # if d is file-path
-    if isinstance(d, (str, Path)):
-        d = check_file_before_read(d)
-        if async_mode is True:
-            return read_image_async(d, formats=formats)
-        else:
-            return ImageModule.open(d, formats=formats)
+    if isinstance(s, (str, Path)):
+        s = check_file_before_read(s)
+        return ImageModule.open(s, formats=formats)
 
     # if d is binary-io
-    if isinstance(d, (RawIOBase, BufferedIOBase)):
-        return ImageModule.open(d)  # type: ignore
+    if isinstance(s, (RawIOBase, BufferedIOBase)):
+        return ImageModule.open(s)  # type: ignore
 
     # if d is bytes
-    d = cast(bytes, d)
-    return ImageModule.open(BytesIO(d), formats=formats)
-
-
-async def read_image_async(f: StrPath, /, *, formats: list[str] | None = None) -> Image:
-    """read image from file (async)"""
-    f = check_file_before_read(f)
-
-    async with async_open(f, 'rb') as fp:
-        bio = BytesIO(await fp.read())
-
-    return ImageModule.open(bio, formats=formats)
+    s = cast(bytes, s)
+    return ImageModule.open(BytesIO(s), formats=formats)
